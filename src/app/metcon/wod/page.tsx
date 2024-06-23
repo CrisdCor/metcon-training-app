508edf139b81b8d7fb4d73bf7d3d87e362bc31fb
Usuario <cristiandavid.corrales@gmail.com>
2024-06-23 15:52:48 -0500

    Avance, maquetado página WOD

--- src/app/metcon/wod/page.tsx
+++ src/app/metcon/wod/page.tsx
@@ -1,12 +1,28 @@
+import Image from "next/image";
+
 import ButtonNav from "@/components/ButtonNav/ButtonNav";
 
 import "./styles.css";
 
 export default function WodPage() {
   return (
-    <div>
-      <h1 className="text-title">Wod Page</h1>
+    <main>
+      <h1 className="text-subtitle text-center">WORK OUT OF THE DAY</h1>
       <ButtonNav path="/metcon/login" text="Regresar" />
-    </div>
+      <div className="wod-wrap">
+        <section className="wod-section section__mobility">
+          <h2>MOBILITY</h2>
+        </section>
+        <section className="wod-section section__mobility">
+          <h2>WARM UP</h2>
+        </section>
+        <section className="wod-section section__mobility">
+          <h2>WEIGHTLIFTING</h2>
+        </section>
+        <section className="wod-section section__mobility">
+          <h2>METCON</h2>
+        </section>
+      </div>
+    </main>
   );
 }
--- src/components/ButtonNav/ButtonNav.tsx
+++ src/components/ButtonNav/ButtonNav.tsx
@@ -8,7 +8,7 @@ interface Props {
   icon?: JSX.Element;
 }
 
-export default function ButtonWod({ path, icon, text }: Props) {
+export default function ButtonNav({ path, icon, text }: Props) {
   return (
     <Link href={path} className="button-base">
       <p className="button-base__text">{text}</p>
